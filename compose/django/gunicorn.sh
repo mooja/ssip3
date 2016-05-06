2f530d18f5bd22557b015a68f758a70d270130b0
Max Shkurygin <max.atreides@gmail.com>
2016-05-06 10:43:02 -0400

    gunicorn now logs to /logs dir

--- compose/django/gunicorn.sh
+++ compose/django/gunicorn.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 python /app/manage.py collectstatic --noinput
 # /usr/local/bin/gunicorn config.wsgi -w 4 -b 0.0.0.0:5000 --chdir=/app
-/usr/local/bin/gunicorn config.wsgi -w 4 -b unix:/run/gunicorn/gunicorn.sock --chdir=/app
+/usr/local/bin/gunicorn config.wsgi\
+    -w 4\
+    -b unix:/run/gunicorn/gunicorn.sock\
+    --error-logfile /logs/gunicorn-errors.log\
+    --access-logfile /logs/gunicorn-access.log\
+    --chdir=/app
