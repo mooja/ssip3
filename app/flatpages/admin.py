38326ea1a4b3f4da9b999696ff56c47759934749
Max Shkurygin <max.atreides@gmail.com>
2016-05-12 14:54:09 -0400

    added flatpages

--- app/flatpages/admin.py
+++ app/flatpages/admin.py
@@ -6,14 +6,13 @@ from ckeditor.widgets import CKEditorWidget
 from .models import FlatPage
 
 
-class FlatPageAdminForm(forms.ModelForm):
-    text = forms.CharField(widget=CKEditorWidget)
-    class Meta:
-        model = FlatPage
-        exclude = ['created']
+# class FlatPageAdminForm(forms.ModelForm):
+#     text = forms.CharField(widget=CKEditorWidget)
+#     class Meta:
+#         model = FlatPage
+#         exclude = ['created']
 
 class FlatPageAdmin(admin.ModelAdmin):
-    form = FlatPageAdminForm
     list_display = ['title']
 
 admin.site.register(FlatPage, FlatPageAdmin)
