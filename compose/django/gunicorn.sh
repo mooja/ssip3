89830b088b5ebb01594e20fba195b103fe7ebe58
Max Shkurygin <max.atreides@gmail.com>
2016-05-16 19:24:42 -0400

    changed to one worker, since only one core on the host

--- compose/django/gunicorn.sh
+++ compose/django/gunicorn.sh
@@ -2,7 +2,7 @@
 python /app/manage.py collectstatic --noinput
 # /usr/local/bin/gunicorn config.wsgi -w 4 -b 0.0.0.0:5000 --chdir=/app
 /usr/local/bin/gunicorn config.wsgi\
-    -w 4\
+    -w 1\
     -b unix:/run/gunicorn/gunicorn.sock\
     --error-logfile /logs/gunicorn-errors.log\
     --access-logfile /logs/gunicorn-access.log\
